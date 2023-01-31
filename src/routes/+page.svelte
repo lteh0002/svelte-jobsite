f44f25acec117df502c47ea2039e6488a8a8754d
lteh0002 <tehlinhui@gmail.com>
2023-01-31 16:58:43 +0800

    Added jobs on site after retrieving api from server

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,3 +4,36 @@
 </script>
 
 <Header />
+<div class="flex flex-col items-center gap-3">
+  {#each data.jobs as job}
+    <div class="card w-4/6 bg-base-100 shadow-xl">
+      <div class="card-body">
+        <h2 class="card-title">{job.title}</h2>
+        <p>{job.employer}</p>
+  
+        <h3 class="font-bold mt-3">{job.location}</h3>
+        <span class="text-sm font-bold">
+          USD {job.minAnnualCompensation} - USD {job.maxAnnualCompensation} yearly
+        </span>
+  
+        <div class="mt-4 ">
+          {job.description.slice(0, 240)}...
+        </div>
+  
+        <div class="italic text-xs opacity-50 mt-2">
+          posted {new Date(job.created).toLocaleDateString(undefined, {
+              weekday: 'long',
+              year: 'numeric',
+              month: 'long',
+              day: 'numeric'
+          })}
+        </div>
+                  
+        <div class="card-actions justify-end">
+          <button class="btn btn-outline">Apply</button>
+        </div>
+      </div>
+    </div>
+  {/each}
+  </div>
+  
