73361531e54d00f6ab72b108e98fb94e051617b7
lteh0002 <tehlinhui@gmail.com>
2023-02-05 22:08:08 +0800

    Remove whitespaces in the middle

--- src/routes/jobs/[slug]/+page.svelte
+++ src/routes/jobs/[slug]/+page.svelte
@@ -35,73 +35,72 @@
             formErrors = res.message
         }
 }
-
 </script>
 
-  <div class="p-5 mb-5">
-    <div class="flex justify-between items-center sm:items-start">
-      <div class="flex flex-col basis-10/12">
+<div class="flex justify-center gap-10 mt-3">
+  <div class="text-justify justify-center mb-5 gap-10">
+    <div class="flex justify-between">
+      <div class="flex flex-col">
         <h1 class="text-xl w-full font-extrabold sm:text-3xl sm:w-full">{data.job.title}</h1>
         <p class="text-sm sm:text-xl">{data.job.employer} <span class="sm:hidden">({data.job.location})</span></p>
       </div>
-
-      <div class="flex items-center">
-        {#if data.job.user == userID}
-        <div class="dropdown dropdown-bottom dropdown-end pr-4 sm:hidden">
-          <label tabindex="0" class="btn btn-circle btn-outline m-1"><i class="fa-solid fa-chevron-down"></i></label>
-          <ul tabindex="0" class="flex flex-col items-center dropdown-content menu p-2 shadow bg-base-100 rounded-box w-20">
-            <li on:click={goUpdate}><i on:click={goUpdate} class='far fa-edit sm:hidden' style='font-size:24px'></i></li>
-            <li on:click={goHomePage}><i on:click={goHomePage} class="fa-solid fa-x" style='font-size:24px'></i></li>
-          </ul>
-        </div>
-        
-        <div class="hidden sm:dropdown sm:dropdown-bottom">
-          <label tabindex="0" class="btn btn-outline m-1">Edit/Delete</label>
-          <ul tabindex="0" class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
-            <li on:click={goUpdate}><a>Edit</a></li>
-            <li on:click={deleteJobs}><a>Delete</a></li>
-          </ul>
-        </div>
-        {/if}
-        <div>
-          <button on:click={goHomePage} class="hidden sm:btn sm:btn-active sm:btn-outline">Back to Homepage</button>
-          {#if data.job.user !== userID}
-          <button on:click={goHomePage} class="btn btn-circle btn-outline mr-3.5 sm:hidden">
+      
+      {#if data.job.user !== userID}
+          <button on:click={goHomePage} class="btn btn-circle btn-outline sm:hidden">
             <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" /></svg>
           </button>
-          {/if}
-        </div>
-        
-      </div>
+      {/if}
     </div>
-
-
     <h2 class="text-xl font-thin mt-3 sm:hidden">Salary Range</h2>
     <p class="mt-1 sm:hidden">
       USD {humanize.formatNumber(data.job.minAnnualCompensation)} - USD
       {humanize.formatNumber( data.job.maxAnnualCompensation ) } yearly
     </p>
-  
-    <div class="flex flex-row mt-4 text-justify justify-between sm:w-full sm:gap-10">
-      <div class="prose max-w-none w-full sm:basis-2/3">
-        <h2 class="text-xl font-thin">Description</h2>
-        <SvelteMarkdown source={data.job.description} />
-        <div class="mt-6" />
-        <h2 class="text-xl font-thin">Requirements</h2>
-        <SvelteMarkdown source={data.job.requirements} />
-        <div class="mt-6"/>
-          <h2 class="text-xl font-thin">How to Apply?</h2>
-          <p class="break-all">{data.job.applicationInstructions}</p>
-        </div>
-        <div class="hidden sm:inline-block ml-4">
-          <h2 class="text-xl font-thin">Location</h2>
-          <p>{data.job.location}</p>
-          <div class="mt-6" />
-          <h2 class="text-xl font-thin">Salary Range</h2>
-          <p>
-            USD {humanize.formatNumber(data.job.minAnnualCompensation)} - USD
-            {humanize.formatNumber( data.job.maxAnnualCompensation )} yearly
-          </p>
-        </div>
+    <div class="flex flex-col prose">
+      <h2 class="text-xl font-thin mt-6">Description</h2>
+      <SvelteMarkdown source={data.job.description} />
+      <div class="mt-6" />
+      <h2 class="text-xl font-thin">Requirements</h2>
+      <SvelteMarkdown source={data.job.requirements} />
+      <div class="mt-6"/>
+        <h2 class="text-xl font-thin">How to Apply?</h2>
+        <p class="break-all">{data.job.applicationInstructions}</p>
       </div>
     </div>
+  
+    <div>
+      <div>
+        <div class="flex items-center justify-between">
+          {#if data.job.user == userID}
+            <div class="dropdown dropdown-bottom dropdown-end pr-4 sm:hidden">
+              <label tabindex="0" class="btn btn-circle btn-outline m-1"><i class="fa-solid fa-chevron-down"></i></label>
+              <ul tabindex="0" class="flex flex-col items-center dropdown-content menu p-2 shadow bg-base-100 rounded-box w-20">
+                <li on:click={goUpdate}><i on:click={goUpdate} class='far fa-edit sm:hidden' style='font-size:24px'></i></li>
+                <li on:click={goHomePage}><i on:click={goHomePage} class="fa-solid fa-x" style='font-size:24px'></i></li>
+              </ul>
+            </div>
+            
+            <div class="hidden sm:dropdown sm:dropdown-bottom">
+              <label tabindex="0" class="btn btn-outline m-1">Edit/Delete</label>
+              <ul tabindex="0" class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
+                <li on:click={goUpdate}><a>Edit</a></li>
+                <li on:click={deleteJobs}><a>Delete</a></li>
+              </ul>
+            </div>
+          {/if}
+          <button on:click={goHomePage} class="hidden sm:btn sm:btn-active sm:btn-outline">Back to Homepage</button>
+        </div>  
+      </div>
+
+      <div class="hidden sm:inline-block">
+        <h2 class="text-xl font-thin mt-9">Location</h2>
+        <p>{data.job.location}</p>
+        <div class="mt-6" />
+        <h2 class="text-xl font-thin">Salary Range</h2>
+        <p>
+          USD {humanize.formatNumber(data.job.minAnnualCompensation)} - USD
+          {humanize.formatNumber( data.job.maxAnnualCompensation )} yearly
+        </p>
+      </div>
+    </div>
+  </div>
