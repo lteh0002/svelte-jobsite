c7b61e0c5b4ca0d57fbcef595ae1005b52edf779
lteh0002 <tehlinhui@gmail.com>
2023-02-06 16:14:52 +0800

    Fix button bug for edit button that directs user to view job details instead of edit job

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,12 +3,21 @@
   import { goto } from '$app/navigation';
 	export let data
 
+  const mq = window.matchMedia("(max-width: 640px)");
+
   function applyJob(id) {
-    goto(`/jobs/${id}`)
+    console.log(event)
+    if (mq.matches && event.target.innerHTML !== 'More Details') {
+      return;
+    } else if (!mq.matches && event.target.innerHTML == 'Edit Job') {
+      return;
+    } else {
+        goto(`/jobs/${id}`)  
+    }
   }
 
   function editJob(id) {
-    goto(`/jobs/${id}/update`)
+      goto(`/jobs/${id}/update`)
   }
 
     let obtainDataFromLocalStorage= localStorage.getItem("auth")
@@ -19,16 +28,20 @@
 <h1 class="text-center text-xl mt-5 font-bold sm:text-3xl">Find Your Next Dream Job Here!</h1>
 <div class="flex flex-col items-center gap-3 mb-10">
   {#each data.jobs as job}
-    <div on:click={applyJob(job.id)} class="card w-5/6 bg-base-100 shadow-xl hover:-translate-y-1 hover:transition hover:delay-75 cursor-pointer">
-      <div class="card-body">
+    <div class="card w-5/6 bg-base-100 shadow-xl hover:-translate-y-1 hover:transition hover:delay-75">
+      <div on:click={applyJob(job.id)} class="card-body sm:cursor-pointer">
         <div class="flex justify-between">
           <div>
             <h2 class="card-title">{job.title}</h2>
             <p>{job.employer}</p>
           </div>
           {#if job.user == userID}
-          <div class="mt-2 sm:hidden">
-            <i on:click={editJob(job.id)} class='far fa-edit cursor-pointer' style='font-size:24px'></i>
+          <div class="mt-2 z-40 sm:hidden">
+            <div class="dropdown dropdown-bottom dropdown-end sm:hidden">
+              <label on:click={editJob(job.id)} tabindex="0" class="btn btn-ghost m-1">
+               <i class='far fa-edit cursor-pointer' style='font-size:24px'></i>
+              </label>
+            </div>    
           </div>
           {/if}
         </div>
@@ -57,9 +70,9 @@
                   
         <div class="card-actions justify-end">
           {#if job.user == userID}
-            <button on:click={editJob(job.id)} class="hidden sm:btn sm:btn-outline mt-2">Edit Job</button>
+            <button on:click|self={editJob(job.id)} class="hidden sm:btn sm:btn-outline mt-2">Edit Job</button>
           {/if}
-          <button on:click={applyJob(job.id)} class="btn btn-outline mt-2 sm:hidden">More Details</button>
+          <button on:click|self={applyJob(job.id)} class="btn btn-outline mt-2 sm:hidden">More Details</button>
         </div>
       </div>
     </div>
