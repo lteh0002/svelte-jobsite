89bdd53a57f5c47f1b3e745649b54824caba4759
lteh0002 <tehlinhui@gmail.com>
2023-02-07 21:19:42 +0800

    Fixed minor typos

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -24,7 +24,7 @@
     <div class="flex justify-center alert alert-warning shadow-lg w-10/12 {$isUserRedirectedFromPostJob == true ? 'block' : 'hidden'} sm:w-5/12">
         <div>
           <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" /></svg>
-          <span>Please Login to Post Jobs!</span>
+          <span>Please Login/Sign Up to Post Jobs!</span>
         </div>
     </div>
 </div>
